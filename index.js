32247730e7ca674bc3d8d85e138dcb9ded1b815c
ChrisAlderson <chris_alderson@hmamail.com>
2016-07-14 21:52:33 +0200

    Removed page filter

--- index.js
+++ index.js
@@ -141,8 +141,6 @@ AnimeApi.prototype.fetch = function (filters) {
     params.sort = filters.sorter;
   }
 
-  filters.page = 1;
-
   var index = 0;
   var url = that.apiURL[index] + 'animes/' + filters.page + '?' + querystring.stringify(params).replace(/%25%20/g, '%20');
   return get(index, url, that).then(formatFetch);
